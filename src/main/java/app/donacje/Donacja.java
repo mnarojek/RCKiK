3e8916e1de93eda6f78cbf29fdf078030f81ba3b
Mateusz <mateusz.narojek@gmail.com>
2021-11-29 19:45:39 +0100

    Dodanie getterow i setterow oraz poprawa pol klasy donacja

--- src/main/java/app/donacje/Donacja.java
+++ src/main/java/app/donacje/Donacja.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "donacja")
 public class Donacja {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -15,9 +16,28 @@ public class Donacja {
     @Column(nullable = false)
     private String type;
 
-    @Column(nullable = false)
-    private Integer id_dawcy;
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
 }
