82446845beabee1157c4cb41cda5224b256c4dae
Mateusz <mateusz.narojek@gmail.com>
2021-11-29 19:45:02 +0100

    Dodanie metody post dla donacji

--- src/main/java/app/donacje/DonacjaController.java
+++ src/main/java/app/donacje/DonacjaController.java
@@ -1,10 +1,12 @@
 package app.donacje;
 
 
+import app.dawca.Dawca;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,6 +25,12 @@ public class DonacjaController {
      public String zalozDonacje(Model model){
         model.addAttribute("donacja", new Donacja());
         return "formularz_donacji";
+    }
+
+    @PostMapping("/donacja/zapisz")
+    public String zapiszDonacje(Donacja donacja){
+        donacjaService.save(donacja);
+        return "redirect:/donacje";
     }
 
 }
